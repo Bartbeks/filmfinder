300fbf3619220839d95df25d1fdb6e2534dc91a0
t-tauri <bart@t-tauri.nl>
2022-12-07 10:36:12 +0100

    versie2

--- js/index.js
+++ js/index.js
@@ -1,95 +1,89 @@
-function addMoviesToDom(movies, id) {
-  // todo remove all node from the dom
-  const map = [];
-  switch (id) {
-    case "latest-movies":
-      movies.map((element) => {
-        removeMoviesFromTheDom();
-        if (element.year >= "2014") {
-          map.push({
-            src: element.poster,
-          });
-        }
-      });
-      break;
-    case "avengers":
-      movies.map((element) => {
-        removeMoviesFromTheDom();
-        let title = element.title.toLowerCase();
-        if (title.includes("avengers")) {
-          map.push({
-            src: element.poster,
-          });
-        }
-      });
-      break;
-    case "x-men":
-      movies.map((element) => {
-        removeMoviesFromTheDom();
-        let title = element.title.toLowerCase();
-        if (title.includes("x-men")) {
-          map.push({
-            src: element.poster,
-          });
-        }
-      });
-      break;
-    case "princess-movies":
-      movies.map((element) => {
-        removeMoviesFromTheDom();
-        let title = element.title.toLowerCase();
-        if (title.includes("princess")) {
-          map.push({
-            src: element.poster,
-          });
-        }
-      });
-      break;
-    case "batman":
-      movies.map((element) => {
-        removeMoviesFromTheDom();
-        let title = element.title.toLowerCase();
-        if (title.includes("batman")) {
-          map.push({
-            src: element.poster,
-          });
-        }
-      });
-      break;
-    default:
-      movies.map((element) => {
-        removeMoviesFromTheDom();
-        map.push({
-          src: element.poster,
-        });
-      });
-  }
-
-  map.forEach((element) => {
+function addMoviesToDom(movies) {
+  movies.forEach((element) => {
+    const movieList = document.getElementById("movieList");
     const node = document.createElement("li");
-    const poster = document.createElement("img");
-    poster.src = element.src;
     node.className = "movie";
+    const link = document.createElement("a");
+    const url = "https://www.imdb.com/title/";
+    link.href = url + element.imdbID;
+    link.target = "_blank";
+    const poster = document.createElement("img");
+    poster.src = element.poster;
+    link.appendChild(poster);
+    node.appendChild(link);
     movieList.appendChild(node);
-    node.appendChild(poster);
   });
 }
 
-// const selectedRadioBtn = document.querySelector('input[name="nav"]:checked').id;
+const movieFilter = (id) => {
+  switch (id) {
+    case "latest-movies":
+      const filteredMovies = movies.filter((movie) => {
+        return movie.year >= "2014";
+      });
+      removeMoviesFromTheDom();
+      addMoviesToDom(filteredMovies);
+      break;
+    case "avengers":
+      const avengerMovies = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes("avengers");
+      });
+
+      addMoviesToDom(avengerMovies);
+      break;
+    case "x-men":
+      const xMenMovies = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes("x-men");
+      });
+
+      addMoviesToDom(xMenMovies);
+      break;
+    case "princess-movies":
+      const princessdMovies = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes("princess");
+      });
+
+      addMoviesToDom(princessdMovies);
+      break;
+
+    case "batman":
+      const BatmanMovies = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes("batman");
+      });
+      addMoviesToDom(BatmanMovies);
+    case "search":
+      const searchValue = document.querySelector("#search");
+      let searchFor = searchValue.value.toLowerCase();
+      const searchResult = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes(searchFor);
+      });
+
+      addMoviesToDom(searchResult);
+      break;
+    default:
+      addMoviesToDom(movies);
+  }
+};
 const selectedRadioBtn = document.querySelectorAll('input[name="nav"]');
-selectedRadioBtn.forEach((element) => {
-  element.addEventListener("click", (getSelectedRadiobtn) => {
-    console.log(element.id);
-    addMoviesToDom(movies, element.id);
+selectedRadioBtn.forEach((radioBtn) => {
+  radioBtn.addEventListener("click", (getSelectedRadiobtn) => {
+    removeMoviesFromTheDom();
+    movieFilter(radioBtn.id);
   });
+});
+
+const searchBtn = document.querySelector("#searchBtn");
+const searchValue = document.querySelector("#search");
+searchBtn.addEventListener("click", (getSearchValue) => {
+  removeMoviesFromTheDom();
+  movieFilter("search");
 });
 
 function removeMoviesFromTheDom() {
   const lstMovie = document.getElementById("movieList");
-  lstMovie.childNodes.forEach((element) => {
-    console.log(element);
-    lstMovie.removeChild(element);
-  });
-}
 
-addMoviesToDom(movies, "default");
+  while (lstMovie.firstChild) {
+    lstMovie.removeChild(lstMovie.firstChild);
+  }
+}
+addMoviesToDom(movies);
